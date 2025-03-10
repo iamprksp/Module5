883aafb302230bb5ec86affb3e8f719773512a13
iamprksp <pyakp@farmingdale.edu>
2025-03-09 20:46:24 -0400

    Add files via upload

---
+++ Unit.java
@@ -0,0 +1,31 @@
+package homework;
+
+/* 1. JUnit is a widely used testing framework for Java that allows developers to write and run automated tests to ensure code correctness.
+ * It's part of the xUnit series for all sorts of languages.
+ * It automated unit testing 
+ * JUnit catches bugs early
+ * 
+ * Annotations used in Junit
+ * @Test – Marks a test method.
+	@BeforeEach – Runs before each test.
+	@AfterEach – Runs after each test.
+	@BeforeAll – Runs once before all tests.
+ * 
+ * 
+ * 
+ * 2.Section 1- 2. Test-Driven Development (TDD) is a software development process where tests are written before implementing functionality.
+	The Red-Green-Refactor cycle:
+	-Write a failing test (Red).
+	-Write minimal code to pass the test (Green).
+	-Refactor the code for optimization (Refactor).
+ * 
+ * Section 2-6
+ * TDD ensures safety when refactoring by keeping tests green
+ * Extracting methods, reducing duplication are common refactoring techniques.
+ * Improving Test Coverage 
+ * -Testing different categories of input.
+ * -Testing edge cases. (e.g 0,1, Integer.MAX_VALUE)
+ */
+
+
+
